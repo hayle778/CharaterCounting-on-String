0b747106e1cd9b50fa9d76668f4606ba12d91504
yourUsername <yourEmail@domain.com>
2020-12-29 14:52:40 -0500

    ff

---
+++ src/Genet.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/*
+Write the program to get the character at the given index with in the string
+*/
+
+// to select the chararcter from the string;
+
+
+public class Genet {
+public static void main(String[]args){
+    String clas = "Haylemelekot Alemneh";
+
+    System.out.println( clas );
+    int please1 = clas.charAt( 0 );
+    int please2 = clas.charAt( 9 );
+
+    System.out.println( "The character at the index one is:" + (char)please1);
+    System.out.println( "The character at the index ten is:" + (char)please2 );
+
+
+}
+
+
+
+
+
+
+
+
+}
